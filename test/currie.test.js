dfdccbea5654a6d41de2a655bb1ae4e9035c1d45
Greg Allen <me@jga.me>
2012-10-01 18:09:47 -0500

    initial commit

---
+++ test/currie.test.js
@@ -0,0 +1,41 @@
+var assert = require('assert');
+
+var currie = require('../');
+
+suite('currie', function() {
+
+  test('basic use', function(done) {
+    var check = false;
+    var test = function(arg1, arg2, arg3, arg4) {
+      check = true;
+
+      assert.equal(arg1, 1);
+      assert.equal(arg2, 2);
+      assert.equal(arg3, 3);
+      assert.equal(arg4, 4);
+
+      done();
+    }
+
+    setTimeout(currie(test, null, 1, 2, 3, 4), 10);
+    assert.equal(check, false);
+  });
+
+  test('prototypes', function(done) {
+
+    var Test = function() {
+
+      setTimeout(currie(this.method, this, 123), 10);
+    }
+    Test.prototype.method = function(arg) {
+      assert.equal(this instanceof Test, true);
+      assert.equal(arg, 123);
+      done();
+    }
+
+    var test = new Test();
+    
+  });
+  
+});
+
