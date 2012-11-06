677388aca84238a424314dc39f580e43224203a5
Greg Allen <me@jga.me>
2012-11-05 18:13:18 -0800

    dist

---
+++ dist/currie.js
@@ -0,0 +1,25 @@
+/*!
+ * currie - a simple proxy/curry function
+ * v0.0.3
+ * https://github.com/jgallen23/currie
+ * copyright JGA 2012
+ * MIT License
+*/
+
+var currie = function(fn, scope) {
+  var args = [];
+  for (var i=2, len = arguments.length; i < len; ++i) {
+    args.push(arguments[i]);
+  }
+  return function() {
+    var fnArgs = args.slice(0);
+    for (var i = 0, c = arguments.length; i < c; i++) {
+      fnArgs.push(arguments[i]);
+    }
+    fn.apply(scope, fnArgs);
+  };
+};
+
+if (typeof module !== 'undefined') {
+  module.exports = currie;
+}
